2c47f69a1a8ec0f137bf2803e0ca425b0f9c8e3c
Anthony Arellano <theanthonyarellano@gmail.com>
2025-01-04 19:15:52 -0500

    utilize json-indexer

--- src/stores/RunTrackingStore.ts
+++ src/stores/RunTrackingStore.ts
@@ -1,9 +1,7 @@
 import { createStore } from "zustand/vanilla";
 import { MetricType } from "~/types/MetricType/MetricType";
 import { Shoe, ShoeMetadata } from "~/types/Shoe/Shoe";
-
-const SHOES_ARRAY_BUFFER = '"shoes":';
-const DEFAULT_CHUNK_SIZE = 1024 * 1024;
+import { JsonIndexer } from "json-indexer";
 
 export type RunTrackingState = {
   fileDownload: string;
@@ -55,108 +53,6 @@ export const defaultInitState: RunTrackingState = {
   isIndexLoading: false
 };
 
-type BaseType = { id: string; filePosition: number; length: number };
-
-type RequiredAdditionalKeys<T> = Exclude<keyof T, keyof BaseType>;
-
-class JsonIndexer<T extends { id: string, filePosition: number, length: number }> {
-    file: File;
-    key: string;
-    additionalIndexKeys: Array<RequiredAdditionalKeys<T>>;
-    chunkSize: number;
-
-    constructor(file: File, key: string, additionalIndexKeys: Array<RequiredAdditionalKeys<T>>, chunkSize = 1024 * 1024) {
-      const missingKeys = this.getMissingKeys(additionalIndexKeys);
-      if (missingKeys.length > 0) {
-        throw new Error(`Missing keys in additionalIndexKeys: ${missingKeys.join(", ")}`);
-      }
-      this.file = file;
-      this.key = `"${key}":`;
-      this.chunkSize = chunkSize;
-      this.additionalIndexKeys = additionalIndexKeys;
-    }
-
-    private getMissingKeys(additionalIndexKeys: Array<keyof T>): Array<keyof T> {
-      const allRequiredKeys: Array<RequiredAdditionalKeys<T>> = Object.keys({} as T).filter(
-        (key) => !['id', 'filePosition', 'length'].includes(key as keyof BaseType)
-      ) as Array<RequiredAdditionalKeys<T>>;
-  
-      return allRequiredKeys.filter((key) => !additionalIndexKeys.includes(key));
-    }
-
-    private extractAdditionalProperties(record: any): Partial<T> {
-      const additionalProperties: Partial<T> = {};
-      for (const key of this.additionalIndexKeys) {
-        if (key in record) {
-          additionalProperties[key] = record[key];
-        }
-      }
-      return additionalProperties;
-    }
-
-    async index(): Promise<Map<string, T>> {
-      let offset = 0;
-      let buffer = '';
-      const index = new Map<string, T>();
-      let inArray = false;
-      let contentPosition = 0;
-
-      while (offset < this.file.size) {
-        const chunk = this.file.slice(offset, offset + this.chunkSize);
-        let text = await chunk.text();
-        console.log(text);
-        buffer += text;
-
-        if (!inArray && buffer.includes(this.key)) {
-          inArray = true;
-          const arrayStart = buffer.indexOf('[', buffer.indexOf(this.key));
-          contentPosition = offset + arrayStart;
-          buffer = buffer.slice(arrayStart);
-        }
-
-        if (inArray) {
-          let bracketCount = 0;
-          let startPos = 0;
-
-          for (let i = 0; i < buffer.length; i++) {
-            const char = buffer[i];
-            
-            if (char === "{") {
-              if (bracketCount === 0) startPos = i;
-              bracketCount++;
-            } else if (char === "}") {
-              bracketCount--;
-              if (bracketCount === 0) {
-                const recordStr = buffer.substring(startPos, i + 1);
-                try {
-                  const record: T = JSON.parse(recordStr);
-                  index.set(record.id, {
-                    id: record.id,
-                    filePosition: contentPosition + startPos,
-                    length: recordStr.length,
-                    ...this.extractAdditionalProperties(record)
-                  } as T);
-                } catch (e) {
-                  console.error('Failed to parse record:', e);
-                }
-              }
-            }
-          }
-          const lastOpenBracket = buffer.lastIndexOf('{');
-          if (lastOpenBracket !== -1) {
-            contentPosition += lastOpenBracket;
-            buffer = buffer.slice(lastOpenBracket);
-          } else {
-            buffer = '';
-          }
-        }
-  
-        offset += this.chunkSize;
-      }
-      return index;
-    }
-}
-
 export const createRunTrackingStore = (
   initState: RunTrackingState = defaultInitState
 ) => {
@@ -167,66 +63,6 @@ export const createRunTrackingStore = (
       try {
         const indexer = new JsonIndexer<ShoeMetadata>(file, 'shoes', ['name']);
         const shoeIndex = await indexer.index();
-        // console.log('TEST SHOE INDEX!', testShoeIndex);
-
-        // let offset = 0;  // For chunking through file
-        // let buffer = '';
-        // const shoeIndex = new Map<string, ShoeMetadata>();
-        // let inShoesArray = false;
-        // let contentPosition = 0;  // Track actual position in file content
-    
-        // while (offset < file.size) {
-        //   const chunk = file.slice(offset, offset + DEFAULT_CHUNK_SIZE);
-        //   const text = await chunk.text();
-        //   buffer += text;
-    
-        //   if (!inShoesArray && buffer.includes(SHOES_ARRAY_BUFFER)) {
-        //     inShoesArray = true;
-        //     const arrayStart = buffer.indexOf('[', buffer.indexOf(SHOES_ARRAY_BUFFER));
-        //     contentPosition = offset + arrayStart;
-        //     buffer = buffer.slice(arrayStart);
-        //   }
-    
-        //   if (inShoesArray) {
-        //     let bracketCount = 0;
-        //     let startPos = 0;
-    
-        //     for (let i = 0; i < buffer.length; i++) {
-        //       const char = buffer[i];
-              
-        //       if (char === "{") {
-        //         if (bracketCount === 0) startPos = i;
-        //         bracketCount++;
-        //       } else if (char === "}") {
-        //         bracketCount--;
-        //         if (bracketCount === 0) {
-        //           const shoeStr = buffer.substring(startPos, i + 1);
-        //           try {
-        //             const shoe = JSON.parse(shoeStr);
-        //             shoeIndex.set(shoe.id, {
-        //               id: shoe.id,
-        //               name: shoe.name,
-        //               filePosition: contentPosition + startPos,
-        //               length: shoeStr.length
-        //             });
-        //           } catch (e) {
-        //             console.error('Failed to parse shoe:', e);
-        //           }
-        //         }
-        //       }
-        //     }
-    
-        //     const lastOpenBracket = buffer.lastIndexOf('{');
-        //     if (lastOpenBracket !== -1) {
-        //       contentPosition += lastOpenBracket;
-        //       buffer = buffer.slice(lastOpenBracket);
-        //     } else {
-        //       buffer = '';
-        //     }
-        //   }
-    
-        //   offset += DEFAULT_CHUNK_SIZE;
-        // }
 
         set({ shoeIndex, originalFile: file });
       } finally {
